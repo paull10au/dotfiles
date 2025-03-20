390312c581a76f1b04a5f5e001c42c121354ba28
paull10au <plucas@salesforce.com>
2025-03-20 22:26:42 +1100

    Create env.zsh

---
+++ env.zsh
@@ -0,0 +1,7 @@
+# JAVA HOME
+# export JAVA_HOME="/Library/Java/Home"
+
+# Fix JAVA HOME for Maven - https://www.marcobehler.com/guides/mvn-clean-install-a-short-guide-to-maven
+#	- https://www.baeldung.com/maven-java-home-jdk-jre
+#	- https://www.baeldung.com/java-home-on-windows-7-8-10-mac-os-x-linux
+export JAVA_HOME=$(/usr/libexec/java_home)
