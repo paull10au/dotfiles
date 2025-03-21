b1df09d6cbbfbe18f54b2cc2832998e87caaaa3c
paull10au <plucas@salesforce.com>
2025-03-21 16:06:47 +1100

    Update path.zsh

--- path.zsh
+++ path.zsh
@@ -20,9 +20,6 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 # Load global Node installed binaries
 # prepend_path "$HOME/.node/bin"
 
-# Load dotfiles binaries
-append_path "$DOTFILES/bin"
-
 # Cumulus CI
 prepend_path "$HOME/.local/bin"
 
@@ -37,6 +34,9 @@ prepend_path "$(brew --prefix python@3.12)/libexec/bin"
 # PATH including local Ruby gems, ie Travis
 # append_path "/.gem/ruby/3.0.0/bin:/Library/Frameworks/Python.framework/Versions/3.6/bin"
 # append_path "/usr/local/opt/ruby/bin"
+
+# Load dotfiles binaries
+append_path "$DOTFILES/bin"
 
 # VS Code
 append_path "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
