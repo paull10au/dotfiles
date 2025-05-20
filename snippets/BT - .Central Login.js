99f8b5fdbbf22958f05212849707cbc1e0a34184
paull10au <plucas@salesforce.com>
2025-05-20 14:34:52 +1000

    Create BT - .Central Login.js

---
+++ snippets/BT - .Central Login.js
@@ -0,0 +1,13 @@
+let $username = document.querySelector("input#username");
+
+$username.value = 'plucas@central.salesforce.com';
+
+// $password.focus();
+// $password.value = '876560349';
+
+window.setTimeout(() => {
+                            let $password = document.querySelector("input#password");
+                            $password.focus();
+                            $password.value = '876560349';
+                        }, 100 );
+
